c70874132a53da31446bf29bdb3be05cda28a331
Zeynep <79228441+zeynepaltay@users.noreply.github.com>
2022-07-20 11:27:47 +0300

    Add files via upload

---
+++ tip-donusumleri/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+byte a=5;
+sbyte b=30;
+short c=10;
+int d=a+b+c;
+Console.WriteLine("d:" + d);
+long h=d;
+Console.WriteLine("h:" + h);
+float i=h;
+Console.WriteLine("i:" + i);
+string e="zeynep";
+char f= 'k';
+object g= e+f+d;
+Console.WriteLine("g:" + g);
